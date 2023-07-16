a79e04b223b2fec144462d7c72917a6d4de51bcf
micael-ricardo <micael.ricardo@outlook.com>
2023-07-16 11:47:47 -0300

    filtro componentizado

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,11 +5,13 @@ import { createApp } from 'vue';
 import ExampleComponent from './components/ExampleComponent.vue';
 import cadastro from './components/cadastro.vue';
 import TabelaEletrodomestico from './components/TabelaEletrodomestico.vue';
+import filtros from './components/filtros.vue';
 
 const app = createApp({});
 
 app.component('cadastro-form', cadastro);
 app.component('tabela-eletrodomestico', TabelaEletrodomestico);
+app.component('filtros-eletrodomestico', filtros);
 app.component('example-component', ExampleComponent);
 
 app.mount('#app');
