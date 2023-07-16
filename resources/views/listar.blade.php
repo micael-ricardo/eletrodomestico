e477edfc5b4df567aba5325c3b66b6b0c5dbc0e8
micael-ricardo <micael.ricardo@outlook.com>
2023-07-16 14:36:19 -0300

    Ajustar rota

--- resources/views/listar.blade.php
+++ resources/views/listar.blade.php
@@ -2,12 +2,19 @@
 @section('title', 'Listar Eletrodomestico')
 @section('conteudo')
 
+<div class="input-group mb-3">
+    <div class="input-group-append">
+        <a   href="{{ route('cadastro') }}" class="btn btn-success"><i class="bi bi-plus"></i> Adicionar</a>
+        <button type="button" class="btn btn-info" id="div-filtro" onclick="return($('#filtro').toggle('fade'))">
+            <i class="bi bi-funnel"></i> Filtros
+        </button>
+    </div>
+</div>
+
 <filtros-eletrodomestico></filtros-eletrodomestico>
 
 <tabela-eletrodomestico></tabela-eletrodomestico>
-
+@endsection
 <script>
     const dataTable = "{{ route('datatable') }}";
 </script>
-
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,11 @@ use App\Http\Controllers\DatatableEletrodomestico;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('cadastro');
+    return view('listar');
 });
 
 Route::get('/datatable', [DatatableEletrodomestico::class, 'datatable'])->name('datatable');
 
 Route::view('/listar', 'listar');
+Route::view('/cadastro', 'cadastro')->name('cadastro');
+
