5ada0c2c29e7563a81e58d4b092623e317fba7da
micael-ricardo <micael.ricardo@outlook.com>
2023-07-16 14:38:54 -0300

    correção componente filtro

--- resources/js/components/filtros.vue
+++ resources/js/components/filtros.vue
@@ -1,42 +1,31 @@
 <template>
-    
-<div class="input-group mb-3">
-    <div class="input-group-append">
-        <a  class="btn btn-success"><i class="bi bi-plus"></i> Adicionar</a>
-        <button type="button" class="btn btn-info" id="div-filtro" onclick="return($('#filtro').toggle('fade'))">
-            <i class="bi bi-funnel"></i> Filtros
-        </button>
-    </div>
-</div>
-<!--  filtros -->
-<div class="panel panel-inverse mb-3" id="filtro" style=" display: none;">
-    <div class="row">
-        <div class="form-group col-sm-2">
-            <label>Data Início:</label>
-            <input type="date" class="form-control" name="data_inicio" id="data_inicio" />
-        </div>
+    <div class="panel panel-inverse mb-3" id="filtro" style=" display: none;">
+        <div class="row">
+            <div class="form-group col-sm-2">
+                <label>Data Início:</label>
+                <input type="date" class="form-control" name="data_inicio" id="data_inicio" />
+            </div>
 
-        <div class="form-group col-sm-2">
-            <label>Data Fim:</label>
-            <input type="date" class="form-control" name="data_fim" id="data_fim" />
-        </div>
-        <div class="form-group col-sm-4">
-            <label>Nome:</label>
-            <input type="text" class="form-control" name="nome" id="nome" />
-        </div>
-        <div class="form-group col-sm-4">
-            <label>Tensão:</label>
-            <input type="text" class="form-control" name="tensao" id="tensao" />
-        </div>
-        <div class="form-group col-sm-4">
-            <label>Descrição:</label>
-            <input type="text" class="form-control" name="descricao" id="descricao" />
-        </div>
-        <div class="form-group col-sm-4">
-            <label>Marca:</label>
-            <input type="text" class="form-control" name="marca" id="marca" />
+            <div class="form-group col-sm-2">
+                <label>Data Fim:</label>
+                <input type="date" class="form-control" name="data_fim" id="data_fim" />
+            </div>
+            <div class="form-group col-sm-4">
+                <label>Nome:</label>
+                <input type="text" class="form-control" name="nome" id="nome" />
+            </div>
+            <div class="form-group col-sm-4">
+                <label>Tensão:</label>
+                <input type="text" class="form-control" name="tensao" id="tensao" />
+            </div>
+            <div class="form-group col-sm-4">
+                <label>Descrição:</label>
+                <input type="text" class="form-control" name="descricao" id="descricao" />
+            </div>
+            <div class="form-group col-sm-4">
+                <label>Marca:</label>
+                <input type="text" class="form-control" name="marca" id="marca" />
+            </div>
         </div>
     </div>
-</div>
-
 </template>
